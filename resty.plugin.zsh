ec392aa2200ea4129fa02efb2364e5b66019f6e7
Adriean Khisbe <adriean.khisbe@live.fr>
2015-02-14 16:55:41 +0100

    introduce plugin resty file for antigen install

---
+++ resty.plugin.zsh
@@ -0,0 +1,18 @@
+##########################################################
+#                                                        #
+#                ____            __                      #
+#               / __ \___  _____/ /___  __               #
+#              / /_/ / _ \/ ___/ __/ / / /               #
+#             / _, _/  __(__  ) /_/ /_/ /                #
+#            /_/ |_|\___/____/\__/\__, /                 #
+#                                /____/                  #
+#                                                        #
+##########################################################
+
+## Zsh Antigen installer for resty.
+
+source "$(dirname $0:A)/resty"
+alias pp="$(cat $(dirname $0:A)/pp)"
+alias pypp="$(cat $(dirname $0:A)/pypp)"
+
+# source file, and create alias with the content of pp, ppyp
